c5b8cbd453beada8787c0929e2163891edf3aba7
Suneel023 <mariochill@yahoo.com>
2020-08-04 18:49:23 +0530

    Second Commit Partials

--- Pro07/urls.py
+++ Pro07/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 import myapp
 from myapp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name="home"),
